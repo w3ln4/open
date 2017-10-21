ba8f54eae76e38fbcc309f1c382e2efd916233b0
w3ln4 <w3ln4@users.noreply.github.com>
2017-10-21 14:32:23 +0200

    REFACTOR: Adjusted batch script to use nmake

--- compile_and_run.bat
+++ compile_and_run.bat
@@ -1,10 +1,4 @@
 @echo off
 call "%VS140COMNTOOLS%"\..\..\VC\bin\amd64\vcvars64
-cl -nologo test.c prod.c /Wall /link
-test.exe
-if %ERRORLEVEL% neq 0 (
-	echo FAILED %ERRORLEVEL%
-) else (
-	echo SUCCESS
-)
+nmake asserter_test
 pause
