049a58fe2cf97aa41ccd0b125ca0001cdc256913
w3ln4 <w3ln4@users.noreply.github.com>
2017-11-11 18:02:36 +0100

    REFACTOR: restored __LINE__ macro for better maintenability and readability

--- asserter/test.c
+++ asserter/test.c
@@ -3,8 +3,8 @@ int asserter_check(int);
 
 int main(void)
 {
-	if (  asserter_check(0 == 0)) return 6;
-	if (! asserter_check(1 == 0)) return 7;
+	if (  asserter_check(0 == 0)) return __LINE__;
+	if (! asserter_check(1 == 0)) return __LINE__;
 	return 0;
 }
 
