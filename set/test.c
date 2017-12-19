5da78e051371b4142d5e4e5b493b8d42c50b79d5
w3ln4 <w3ln4@users.noreply.github.com>
2017-12-19 22:08:02 +0100

    [SET] FAIL: get size of empty set

--- set/test.c
+++ set/test.c
@@ -1,10 +1,12 @@
 
 int asserter_check(int);
 int set_empty(void);
+int set_size(int);
 
 int main(void)
 {
 	if (asserter_check(set_empty() == 0)) return __LINE__;
+	if (asserter_check(set_size(set_empty()) == 0)) return __LINE__;
 	return 0;
 }
 
