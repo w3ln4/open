8eecd121ce5c3ff538497c51db35d1006460afb2
w3ln4 <w3ln4@users.noreply.github.com>
2017-12-20 21:34:17 +0100

    [SET] FAIL: Adding elements creates non empty set

--- set/test.c
+++ set/test.c
@@ -2,11 +2,13 @@
 int asserter_check(int);
 int set_empty(void);
 int set_size(int);
+int set_add(int, int);
 
 int main(void)
 {
 	if (asserter_check(set_empty() == 0)) return __LINE__;
 	if (asserter_check(set_size(set_empty()) == 0)) return __LINE__;
+	if (asserter_check(set_add(set_empty(), 1) != set_empty())) return __LINE__;
 	return 0;
 }
 
