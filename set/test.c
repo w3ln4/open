6f0283d3bbb74c99b7cd4127918c569467810d9e
w3ln4 <w3ln4@users.noreply.github.com>
2018-01-02 17:41:45 +0100

    [SET] FAIL: size of one element set is not zero

--- set/test.c
+++ set/test.c
@@ -6,9 +6,15 @@ int set_add(int, int);
 
 int main(void)
 {
+	// set_empty tests
 	if (asserter_is_false(set_empty() == 0)) return __LINE__;
-	if (asserter_is_false(set_size(set_empty()) == 0)) return __LINE__;
+
+	// set_add tests
 	if (asserter_is_false(set_add(set_empty(), 1) != set_empty())) return __LINE__;
+
+	// set_size tests
+	if (asserter_is_false(set_size(set_empty()) == 0)) return __LINE__;
+	if (asserter_is_false(set_size(set_add(set_empty(), 1)) == 1)) return __LINE__;
 	return 0;
 }
 
