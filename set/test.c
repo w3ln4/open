5ae0ca2a1b38502b395b4f83fafe53a2f78a47ff
w3ln4 <w3ln4@users.noreply.github.com>
2018-01-02 17:54:26 +0100

    [SET] FAIL: zero may be member of set

--- set/test.c
+++ set/test.c
@@ -10,7 +10,7 @@ int main(void)
 	if (asserter_is_false(set_empty() == 0)) return __LINE__;
 
 	// set_add tests
-	if (asserter_is_false(set_add(set_empty(), 1) != set_empty())) return __LINE__;
+	if (asserter_is_false(set_add(set_empty(), 0) != set_empty())) return __LINE__;
 	if (asserter_is_false(set_add(set_empty(), 1) != set_add(set_empty(), 2))) return __LINE__;
 
 	// set_size tests
