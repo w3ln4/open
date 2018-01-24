585a9ff6ea3558ce200a2b1eb19a6ea16888a4e1
w3ln4 <w3ln4@users.noreply.github.com>
2018-01-24 21:52:59 +0100

    [RUNNER] FAIL: force to use input parameter

--- runner/test.c
+++ runner/test.c
@@ -1,14 +1,20 @@
 
 int runner_start(int (*func)(void));
 
-static int _runner_test_func(void)
+static int _runner_test_func_0(void)
 {
 	return 0;
 }
 
+static int _runner_test_func_1(void)
+{
+	return 1;
+}
+
 int main(void)
 {
-	if (runner_start(_runner_test_func) != 0) return 1;
+	if (runner_start(_runner_test_func_0) != 0) return 1;
+	if (runner_start(_runner_test_func_1) != 1) return 2;
 	return 0;
 }
 
