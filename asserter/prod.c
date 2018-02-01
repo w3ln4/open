0704197a7b76af68545b09e1d4e96337fd116481
w3ln4 <w3ln4@users.noreply.github.com>
2018-02-01 16:59:34 +0100

    [ASSERTER] REFACTOR: asserter_crash_if_false -> asserter_crash_if

--- asserter/prod.c
+++ asserter/prod.c
@@ -1,18 +1,17 @@
 void runner_crash(int);
 
-void asserter_crash_if_false(int condition, int return_code)
+void asserter_crash_if(int condition, int return_code)
 {
-	if (condition) return;
-	runner_crash(return_code);
+	if (condition) runner_crash(return_code);
 }
 
 void asserter_crash_if_equal(int lvalue, int rvalue, int return_code)
 {
-	asserter_crash_if_false(lvalue != rvalue, return_code);
+	asserter_crash_if(lvalue == rvalue, return_code);
 }
 
 void asserter_crash_if_not_equal(int lvalue, int rvalue, int return_code)
 {
-	asserter_crash_if_false(lvalue == rvalue, return_code);
+	asserter_crash_if(lvalue != rvalue, return_code);
 }
 
--- asserter/test.c
+++ asserter/test.c
@@ -1,10 +1,10 @@
 
-void asserter_crash_if_false(int, int);
+void asserter_crash_if(int, int);
 
 int runner_main(void)
 {
-	asserter_crash_if_false(0 == 0, __LINE__);
-	asserter_crash_if_false(1 == 0, 0);
+	asserter_crash_if(1 == 0, __LINE__);
+	asserter_crash_if(0 == 0, 0);
 	return __LINE__;
 }
 
