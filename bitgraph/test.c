d0350b04174fe1aa9af4dff3a55972cf69c49ef2
w3ln4 <w3ln4@users.noreply.github.com>
2018-02-20 20:13:47 +0100

    [BITGRAPH] FAIL: bitgraph_edge_add

--- bitgraph/test.c
+++ bitgraph/test.c
@@ -1,11 +1,16 @@
 
 void asserter_crash_if_not_equal(int, int, int);
 unsigned int bitgraph_empty(void);
+unsigned int bitgraph_edge_add(unsigned int, unsigned int, unsigned int);
 
 int runner_main(void)
 {
 	// bitgraph_empty tests
 	asserter_crash_if_not_equal(bitgraph_empty(), 0, __LINE__);
+
+	// bitgraph_edge_add tests
+	asserter_crash_if_equal(bitgraph_edge_add(bitgraph_empty(), 0, 0), bitgraph_empty());
+
 	return 0;
 }
 
