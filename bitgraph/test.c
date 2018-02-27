a91f49fdab37f761209a38645aa9e6834b560c93
w3ln4 <w3ln4@users.noreply.github.com>
2018-02-27 20:37:06 +0100

    [BITGRAPH] FAIL: bitgraph_edge_set initial test

--- bitgraph/test.c
+++ bitgraph/test.c
@@ -1,7 +1,11 @@
 
 void asserter_crash_if_not_equal(int, int, int);
+void asserter_crash_if_equal(int, int, int);
+unsigned int bitset_empty(void);
+
 unsigned int bitgraph_empty(void);
 unsigned int bitgraph_edge_add(unsigned int, unsigned int, unsigned int);
+unsigned int bitgraph_edge_set(unsigned int, unsigned int);
 
 int runner_main(void)
 {
@@ -9,7 +13,10 @@ int runner_main(void)
 	asserter_crash_if_not_equal(bitgraph_empty(), 0, __LINE__);
 
 	// bitgraph_edge_add tests
-	asserter_crash_if_equal(bitgraph_edge_add(bitgraph_empty(), 0, 0), bitgraph_empty());
+	asserter_crash_if_equal(bitgraph_edge_add(bitgraph_empty(), 0, 0), bitgraph_empty(), __LINE__);
+
+	// bitgraph_edge_set tests
+	asserter_crash_if_not_equal(bitgraph_edge_set(bitgraph_empty(), 0), bitset_empty(), __LINE__);
 
 	return 0;
 }
