9918760d44d62e4175d7bfd38b643afd4ab7fd8c
w3ln4 <w3ln4@users.noreply.github.com>
2018-02-27 20:40:02 +0100

    [BITGRAPH] PASS: bitgraph_edge_set initial test

--- bitgraph/prod.c
+++ bitgraph/prod.c
@@ -7,3 +7,9 @@ unsigned int bitgraph_edge_add(unsigned int graph, unsigned int node1, unsigned 
 {
 	return 1;
 }
+
+unsigned int bitgraph_edge_set(unsigned int graph, unsigned int node)
+{
+	return 0;
+}
+
