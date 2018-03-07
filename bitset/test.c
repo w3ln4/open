19ef221158d7cf1ee0fdef2aea45b8e917f6018f
w3ln4 <w3ln4@users.noreply.github.com>
2018-03-07 23:09:40 +0100

    [BITSET] FAIL: bitset_contains should return true if element in set

--- bitset/test.c
+++ bitset/test.c
@@ -30,6 +30,7 @@ int runner_main(void)
 
 	// bitset_contains tests
 	asserter_crash_if(bitset_contains(bitset_empty(), 0), __LINE__);
+	asserter_crash_if(! bitset_contains(bitset_add(bitset_empty(), 0), 0), __LINE__);
 
 	return 0;
 }
