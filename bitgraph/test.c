cae395f14c8185feebaf85e4e91d4387ba3c317a
w3ln4 <w3ln4@users.noreply.github.com>
2018-03-07 15:26:58 +0100

    [BITGRAPH] FAIL: check if neighbor is present in returned set

--- bitgraph/test.c
+++ bitgraph/test.c
@@ -1,7 +1,9 @@
 
+void asserter_crash_if(int, int);
 void asserter_crash_if_not_equal(int, int, int);
 void asserter_crash_if_equal(int, int, int);
 unsigned int bitset_empty(void);
+int bitset_is_in(unsigned int, unsigned int);
 
 unsigned int bitgraph_empty(void);
 unsigned int bitgraph_edge_add(unsigned int, unsigned int, unsigned int);
@@ -18,6 +20,7 @@ int runner_main(void)
 	// bitgraph_neighbors_get tests
 	asserter_crash_if_not_equal(bitgraph_neighbors_get(bitgraph_empty(), 0), bitset_empty(), __LINE__);
 	asserter_crash_if_equal(bitgraph_neighbors_get(bitgraph_edge_add(bitgraph_empty(), 0, 0), 0), bitset_empty(), __LINE__);
+	asserter_crash_if(! bitset_is_in(bitgraph_neighbors_get(bitgraph_edge_add(bitgraph_empty(), 0, 1), 0), 1), __LINE__);
 
 	return 0;
 }
