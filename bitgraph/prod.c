11ceaadf196919be14c398cad0c6ca6037ae9893
w3ln4 <w3ln4@users.noreply.github.com>
2018-03-10 00:59:26 +0100

    [BITGRAPH] PASS: adding different edges creates different graphs

--- bitgraph/prod.c
+++ bitgraph/prod.c
@@ -5,7 +5,7 @@ unsigned int bitgraph_empty(void)
 
 unsigned int bitgraph_edge_add(unsigned int graph, unsigned int node1, unsigned int node2)
 {
-	return 1;
+	return 1 + node1 + node2;
 }
 
 unsigned int bitgraph_neighbors_get(unsigned int graph, unsigned int node)
