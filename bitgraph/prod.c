1caf40c95476774773e568f075155cf15eeb511e
w3ln4 <w3ln4@users.noreply.github.com>
2018-03-25 16:15:40 +0200

    [BITGRAPH] PASS: many neighbors test

--- bitgraph/prod.c
+++ bitgraph/prod.c
@@ -1,3 +1,6 @@
+unsigned int bitset_empty();
+unsigned int bitset_add(unsigned int, unsigned int);
+
 unsigned int bitgraph_empty(void)
 {
 	return 0;
@@ -10,7 +13,7 @@ unsigned int bitgraph_edge_add(unsigned int graph, unsigned int node1, unsigned 
 
 unsigned int bitgraph_neighbors_get(unsigned int graph, unsigned int node)
 {
-	if (graph) return 2;
+	if (graph) return bitset_add(bitset_add(bitset_add(bitset_empty(), 0), 1), 3);
 	return 0;
 }
 
